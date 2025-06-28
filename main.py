94495f0255abd822d027fe961129cdc70203bea8
haozhang <75785473+nclabhzhang@users.noreply.github.com>
2025-06-28 21:18:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 from joblib import dump
 from scipy.stats import pearsonr
 from math import isnan
@@ -25,14 +24,12 @@ seed = 42
 time_stamp = timestamp() + '_' + 's2398_seed' + str(seed)
 dataset = time_stamp.split('_')[1][-4:]
 
-writer = SummaryWriter('logs')
 init_model_path = './ckpt/' + time_stamp + '/mffen_model'
 init_opt_path = './optimizers/' + time_stamp + '/opt_DGG'
 save_path = './ckpt/' + time_stamp + '/mffen_model'
 opt_path = './optimizers/' + time_stamp + '/opt_DGG'
 result_path = './result/' + time_stamp + '/'
 make_save_dir(time_stamp)
-write_result(time_stamp, 'main_skempi.py')
 
 
 def train_cross_validation(model, fold_i, train_loader, test_loader):
@@ -41,7 +38,7 @@ def train_cross_validation(model, fold_i, train_loader, test_loader):
     epoch = 0
     not_best_count = 0
     best_pcc, best_rmse = 0.0, 100.0
-    train_rmsd_numpy, test_rmsd_numpy, test_pcc_numpy = np.zeros((max_epoches)), np.zeros((max_epoches)), np.zeros((max_epoches))
+    train_rsme_numpy, test_rsme_numpy, test_pcc_numpy = np.zeros((max_epoches)), np.zeros((max_epoches)), np.zeros((max_epoches))
     model = model.to(device)
     lossfunc = torch.nn.MSELoss()
     while epoch < max_epoches:
@@ -57,17 +54,17 @@ def train_cross_validation(model, fold_i, train_loader, test_loader):
             avg_loss.update(loss.item(), affinity.shape[0])
             sys.stdout.write('\r[Fold %d][Epoch %d] Train | step: %d | loss: %f, avg_loss: %f' % (fold_i, epoch + 1, batch_id + 1, torch.sqrt(loss), np.sqrt(avg_loss.avg)))
         train_sqrt_loss = np.sqrt(avg_loss.avg)
-        train_rmsd_numpy[epoch] = train_sqrt_loss
+        train_rsme_numpy[epoch] = train_sqrt_loss
 
         if (epoch + 1) % val_iter == 0:
-            test_rmsd, test_pcc_value, affinity_label, affinity_predict = test_pcc(model, test_loader)
-            test_rmsd_numpy[epoch] = test_rmsd
+            test_rsme, test_pcc_value, affinity_label, affinity_predict = test_pcc(model, test_loader)
+            test_rsme_numpy[epoch] = test_rsme
             test_pcc_numpy[epoch] = test_pcc_value
-            print('\n[TEST] seed = %d, pcc: %f, rmse: %f' % (seed, test_pcc_value, test_rmsd))
-            if test_pcc_value >= best_pcc and test_rmsd <= best_rmse:
+            print('\n[TEST] seed = %d, pcc: %f, rmse: %f' % (seed, test_pcc_value, test_rsme))
+            if test_pcc_value >= best_pcc and test_rsme <= best_rmse:
                 print('Best result (epoch %d) !!!' % epoch)
                 best_pcc = test_pcc_value
-                best_rmse = test_rmsd
+                best_rmse = test_rsme
                 torch.save(model, save_path + '_fold' + str(fold_i))
                 torch.save(optimizer, opt_path + '_fold' + str(fold_i))
                 not_best_count = 0
@@ -76,10 +73,8 @@ def train_cross_validation(model, fold_i, train_loader, test_loader):
             if not_best_count >= early_stop:
                 break
         epoch += 1
-    dump((train_rmsd_numpy, test_rmsd_numpy, test_pcc_numpy), result_path + 'cross_validation_fold' + str(fold_i))
-    return train_rmsd_numpy, test_rmsd_numpy, test_pcc_numpy, best_pcc, best_rmse
-
-
+    dump((train_rsme_numpy, test_rsme_numpy, test_pcc_numpy), result_path + 'cross_validation_fold' + str(fold_i))
+    return train_rsme_numpy, test_rsme_numpy, test_pcc_numpy, best_pcc, best_rmse
 
 
 def test_pcc(model, test_loader):
@@ -114,7 +109,7 @@ def init_model_optimizer():
 
 if __name__ == '__main__':
     init_model_optimizer()
-    train_rmsd_10fold, test_rmsd_10fold, test_pcc_10fold = [], [], []
+    train_rsme_10fold, test_rsme_10fold, test_pcc_10fold = [], [], []
     res_best_pcc, res_best_rmse = [], []
     for fold_i in range(10):   # begin ten-fold cross validation training
         set_random_seed(seed)
@@ -135,9 +130,9 @@ if __name__ == '__main__':
         optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=1e-6)
         optimizer.load_state_dict(torch.load(init_opt_path + '_good_model_seed' + str(seed) + '_init'))
 
-        train_rmsd_numpy, test_rmsd_numpy, test_pcc_numpy, best_pcc, best_rmse = train_cross_validation(model, fold_i, train_loader, test_loader)
-        train_rmsd_10fold.append(train_rmsd_numpy)
-        test_rmsd_10fold.append(test_rmsd_numpy)
+        train_rsme_numpy, test_rsme_numpy, test_pcc_numpy, best_pcc, best_rmse = train_cross_validation(model, fold_i, train_loader, test_loader)
+        train_rsme_10fold.append(train_rsme_numpy)
+        test_rsme_10fold.append(test_rsme_numpy)
         test_pcc_10fold.append(test_pcc_numpy)
         res_best_pcc.append(best_pcc)
         res_best_rmse.append(best_rmse)
@@ -146,5 +141,4 @@ if __name__ == '__main__':
         torch.cuda.empty_cache()
 
     print('Final result avg | pcc: %f, rmse: %f' % (sum(res_best_pcc) / len(res_best_pcc), sum(res_best_rmse) / len(res_best_rmse)))
-    dump((train_rmsd_10fold, test_rmsd_10fold, test_pcc_10fold), result_path + '10fold_cross_validation_10fold_result')
-    writer.close()
+    dump((train_rsme_10fold, test_rsme_10fold, test_pcc_10fold), result_path + '10fold_cross_validation_result')
